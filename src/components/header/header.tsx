09c71236c172d30d5a61f9b0ac9c0fc04325b46d
Dimativ <Dimativtoptinker@gmail.com>
2023-09-17 22:54:01 +0300

    feat: added responsive breeds and gallery page feat: added responsive likes, dislikes and favourite menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,16 +2,24 @@ import style from "./header.module.scss"
 import {Link, useLocation} from "react-router-dom";
 import logo from "../../pics/header/logo.png"
 import {SearchAndNav} from "../../index.ts";
-const Header = () => {
-    const location = useLocation()
+
+const LogoAndTitle = () => {
     return (
-        <header className={style.wrapper}>
+        <>
             <Link to="/" className={style.logo}>
                 <img src={logo}></img>
             </Link>
             <Link to="/" className={style.nameText}>
                 <h1>RatePets</h1>
             </Link>
+        </>
+    )
+}
+const Header = () => {
+    const location = useLocation()
+    return (
+        <header className={style.wrapper}>
+            <LogoAndTitle/>
             {location.pathname !== '/' && <SearchAndNav location={location}/>}
         </header>
     )
